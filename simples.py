553f91202ff69305a2b5f73e297996e194850efe
Matheus Muller <matheusmiiller44@gmail.com>
2025-01-15 20:44:07 -0300

    implementando funcionalidade basica em simples.py

--- simples.py
+++ simples.py
@@ -1,3 +1,15 @@
-number_1 = input('Digite o primeiro número: ')
-number_2 = input('Digite o segunfo número: ')
+number_1 = float(input('Digite o primeiro número: '))
+number_2 = float(input('Digite o segunfo número: '))
 
+# adição
+print('{} + {} = '.format(number_1, number_2))
+print(number_1 + number_2)
+# subtração
+print('{} - {} = '.format(number_1, number_2))
+print(number_1 - number_2)
+# multiplicação
+print('{} * {} = '.format(number_1, number_2))
+print(number_1 * number_2)
+# Divisão
+print('{} / {} = '.format(number_1, number_2))
+print(number_1 / number_2)
